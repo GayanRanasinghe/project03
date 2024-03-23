ada5153b5f5d96265de56e103e572b56e50be2e2
gayanranasinghe <gayanmf@gmail.com>
2024-03-23 14:49:38 -0600

    Layout changes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,15 +4,14 @@
       <q-toolbar class="bg-primary text-white shadow-2">
       <q-btn flat round dense icon="menu" class="q-mr-sm" />
       <q-separator dark vertical inset />
-      <q-btn stretch flat label="Link" />
 
       <q-space />
 
       
       <q-separator dark vertical />
-      <q-btn stretch flat label="Link" />
+      <q-btn stretch flat label="About" />
       <q-separator dark vertical />
-      <q-btn stretch flat label="Link" />
+      <q-btn stretch flat label="Blog" />
     </q-toolbar>
       <router-view />
     </q-page-container>
